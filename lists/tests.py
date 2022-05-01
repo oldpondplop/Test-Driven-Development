3eda92a4d3fb4dc0880efd4fb6e9cd42b3eade2d
ntixitho <ntixitho@protonmail.com>
2022-05-01 17:25:11 +0300

    revert to 8a20d19960f54

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
-from lists.models import Item, List
+from django.urls import resolve
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
+from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -41,24 +46,16 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
 
 
-class ListAndItemModelTest(TestCase):
+class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
         first_item = Item()
         first_item.text = 'The first (ever) list item'
-        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
-        second_item.list = list_
         second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -66,6 +63,4 @@ class ListAndItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
