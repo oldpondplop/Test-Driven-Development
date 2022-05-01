4441716ad1ff60043e8ac82ebbe25cc01ddc9136
ntixitho <ntixitho@protonmail.com>
2022-05-01 15:29:09 +0300

    test multiple users can start lists at different urls

--- lists/tests.py
+++ lists/tests.py
@@ -9,14 +9,6 @@ from lists.models import Item
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
