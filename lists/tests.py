406848fdbf32c3df5ffbddc320acd54d28b89dae
ntixitho <ntixitho@protonmail.com>
2022-05-01 20:01:58 +0300

    migrations + tests

--- lists/tests.py
+++ lists/tests.py
@@ -25,6 +25,7 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
+
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
@@ -32,8 +33,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
